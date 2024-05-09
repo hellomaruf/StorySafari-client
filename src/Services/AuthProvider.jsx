fadc18533dfb94f97699607e3a2da79b5e3fe3d9
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-10 03:06:39 +0600

    Added observer

--- src/Services/AuthProvider.jsx
+++ src/Services/AuthProvider.jsx
@@ -1,16 +1,17 @@
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
 } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "./firebase.config";
-import { GoogleAuthProvider,GithubAuthProvider } from "firebase/auth";
-
+import { GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 export const AuthContext = createContext();
 function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
   // create a user
   const createNewUser = (email, password) => {
     setLoading(true);
@@ -30,12 +31,26 @@ function AuthProvider({ children }) {
     return signInWithPopup(auth, googleProvider);
   };
 
-  // Github login 
+  // Github login
   const githubLogin = () => {
-    setLoading(true)
-    const githubProvider = new GithubAuthProvider()
-    return signInWithPopup(auth, githubProvider)
-  }
+    setLoading(true);
+    const githubProvider = new GithubAuthProvider();
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  //Observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        console.log("Current user ---->", currentUser);
+      }
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   const authInfo = {
     createNewUser,
     signInUser,
